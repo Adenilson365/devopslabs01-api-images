dd3ea7dc91337f2aa76c516123886af77218f9eb
adenilsonalves_compasso <adenilsonalves_compasso@tupperware.com>
2024-12-30 11:40:04 -0300

    feat: Adiciona loggin simples

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, send_file, jsonify
 import os
+import logging
 from flask_cors import CORS;
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(asctime)s - %(message)s')
 
 app = Flask(__name__)
 CORS(app )
@@ -12,17 +14,19 @@ os.makedirs(IMAGE_DIR, exist_ok=True)
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
+        logging.error("Nenhuma imagem enviada")
         return jsonify({'error': 'Nenhuma imagem enviada'}), 400
     
     image = request.files['image']
     if image.filename == '':
+        logging.error("Nome de arquivo Inválido")
         return jsonify({'error': 'Nome de arquivo inválido'}), 400
     
     # Salva a imagem no diretório de imagens do container
     image_path = os.path.join(IMAGE_DIR, image.filename)
     print(image_path)
     image.save(image_path)
-    
+    logging.info("Imagem salva com sucesso")
     return jsonify({'message': 'Imagem salva com sucesso', 'image_id': image.filename}), 201
 
 @app.route('/get-image/<string:image_id>', methods=['GET'])
@@ -32,9 +36,11 @@ def get_image(image_id):
         image_path = os.path.join(IMAGE_DIR, image_id)
         print(image_path)
         if not os.path.exists(image_path):
+            logging.error("Imagem não encontrada")
             return jsonify({'error': 'Imagem não encontrada'}), 404
         return send_file(image_path, mimetype='image/png')
     except Exception as e:
+        logging.error("Erro ao carregar imagem")
         return jsonify({'error': f'Erro ao carregar a imagem: {str(e)}'}), 500
 
 if __name__ == "__main__":
